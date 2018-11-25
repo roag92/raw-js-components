8bd361b6889805ccc6a6613b503a6594ee15fd94
Ricardo Román <ricardo.roman.ag@gmail.com>
2018-11-24 23:51:48 -0600

    Refactored app.js and index.html

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 (function() {
   'use stricts';
 
-  var searchElement = document.getElementById('search');
-  var listElement = document.getElementById('list');
-  var viewElement = document.getElementById('view');
-
-  var viewComponent = new ViewComponent(viewElement);
-  var listComponent = new ListComponent(listElement, viewComponent);
-  var searchComponent = new SearchComponent(searchElement, listComponent);
-
-  console.log(viewComponent);
-  console.log(listComponent);
-  console.log(searchComponent);
+  new SearchComponent(
+    document.getElementById('search'),
+    new ListComponent(
+      document.getElementById('list'),
+      new ViewComponent(document.getElementById('view'))
+    )
+  );
 })();
