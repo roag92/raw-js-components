b6e6eb71afb9b0fd468a2ee02f4ddf6bcd930d0f
Ricardo Roman <ricardo.roman.ag@gmail.com>
2020-03-09 11:37:18 -0600

    chore: prepare deps for docker template

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,21 @@
+var isDocker = require('is-docker')();
+var puppeteer = require('puppeteer');
+
+process.env.CHROME_BIN = puppeteer.executablePath();
+
 module.exports = function(config) {
-    config.set({
-      basePath: './',
-      frameworks: ['jasmine', 'sinon'],
-      browsers: ['ChromeHeadless'],
-      files: [
-          'src/components/**/*.js',
-          'tests/**/*spec.js'
-      ],
-      autoWatch: false,
-      singleRun: true
-    });
-  };
+  config.set({
+    basePath: './',
+    frameworks: ['jasmine', 'sinon'],
+    browsers: ['ChromeCustom'],
+    files: ['src/components/**/*.js', 'tests/**/*spec.js'],
+    autoWatch: false,
+    singleRun: true,
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'ChromeHeadless',
+        flags: isDocker ? ['--no-sandbox'] : [],
+      },
+    },
+  });
+};
